a0b7e2f2a1280568b2f2b774eba1603757ce1aa9
atharvanaphade <naphade21@gmail.com>
2021-04-11 00:16:26 +0530

    Added Models

--- ctd_website/users/models.py
+++ ctd_website/users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    college = models.CharField(max_length=255)
+    country_code = models.CharField(max_length=4)
+    phone_no = models.CharField(max_length=10)
+    junior = models.BooleanField(default=False)
+
+class Events(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.IntegerField(default=0)
+
+class Orders(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    event_id = models.ForeignKey(Events, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.event_id.name
+
+
+
+
+
