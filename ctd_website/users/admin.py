71a1584501e79562110cf88c147f26ddfa6b5fdf
Neil Deshpande <neildeshpande10@gmail.com>
2021-04-11 15:33:45 +0530

    Changed server timezone. Updated models according to functional requirements. Registered models in admin panel.

--- ctd_website/users/admin.py
+++ ctd_website/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import (
+    Profile,
+    EventLine,
+    Event,
+    Order,
+    MultipleEventsOrder,
+)
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(EventLine)
+admin.site.register(Event)
+admin.site.register(Order)
+admin.site.register(MultipleEventsOrder)
--- ctd_website/users/models.py
+++ ctd_website/users/models.py
@@ -5,24 +5,50 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    college = models.CharField(max_length=255)
     country_code = models.CharField(max_length=4)
     phone_no = models.CharField(max_length=10)
-    junior = models.BooleanField(default=False)
+    # college = models.CharField(max_length=255) --> not needed since this is an intra college event
+    reg_no = models.CharField(max_length=11) # asking for clg reg no to ensure 'pictians only' participation
+    senior = models.BooleanField(default=False)
 
-class Events(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
-    price = models.IntegerField(default=0)
+# event line can have entries such as 'technical', 'non-technical', etc.
+# better to have it just in case we add other type of events in the future
+# database will be more ordered
 
-class Orders(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    event_id = models.ForeignKey(Events, on_delete=models.CASCADE)
+class EventLine(models.Model):
+    event_line_name = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.event_id.name
+class Event(models.Model):
+    event_line_fk = models.ForeignKey(EventLine, on_delete=models.CASCADE)
+    event_name = models.CharField(max_length=255)
+    event_description = models.CharField(max_length=255)
+    price_for_junior = models.IntegerField(default=0)
+    price_for_senior = models.IntegerField(default=0)
+    event_start_date = models.DateTimeField()
+    event_end_date = models.DateTimeField()
+
+class Order(models.Model):
+
+    payment_methods = [
+        ('cc', 'credit_card'),
+        ('dc', 'debit_card'),
+        ('upi', 'upi'),
+    ]
+
+    user_id_fk = models.ForeignKey(User, on_delete=models.CASCADE)
+    # event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+    order_date_time = models.DateTimeField(auto_now_add=True) # automatically saved when object is first created
+    payment_method = models.CharField(max_length=3, choices=payment_methods, default='upi')
+    payment_status = models.IntegerField(default=2) # 0 for failed, 1 for successful, 2 for pending
+    payment_date_time = models.DateTimeField()
+
+    # might require additional fields based on the payment api used
 
 
+# one order by a user can have multiple events in it
+# all those registration for events must come under same order id, hence the separate model
 
-
-
+class MultipleEventsOrder(models.Model):
+    order_id_fk = models.ForeignKey(Order, on_delete=models.CASCADE)
+    event_id_fk = models.ForeignKey(Event, on_delete=models.CASCADE)
+    event_password = models.CharField(max_length=15) # unique auto generated password for each entry
